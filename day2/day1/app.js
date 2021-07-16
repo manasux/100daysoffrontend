7c9c878601d8e0b8b68d67d1dc51d910cc86092c
manasux <amr13mk@gmail.com>
2021-07-16 11:34:26 +0530

    1st challenge

---
+++ day2/day1/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+window.addEventListener('keydown', function(e) {
+
+    const audio = document.querySelector(`audio[data-key= "${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+});
